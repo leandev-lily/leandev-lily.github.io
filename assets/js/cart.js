60ec4b554c638f8e9d8ddadcb80dc4934f2c094d
lilzha <lily.zhang@leandev.se>
2019-04-12 16:25:27 +0800

    initial commit -6

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,10 +1,23 @@
-var products = [{
-    "name":"LOREM IPSUM DOLOR 15",
-    "price":"33900"
-},
-    {
 
-    }];
+jQuery(document).ready(function () {
+    updateQuatityInCart();
+});
+
+function updateQuatityInCart() {
+    var products = window.localStorage.getItem("products");
+    console.log(products);
+    if(products == 'undefined' || products == ''){
+        products = null;
+        window.localStorage.clear();
+    }
+    if(products != null && products.indexOf(',') != -1){
+        jQuery('#itemQuality').html(products.split(",").length);
+    }else if(products != null && products.trim() != ''){
+        jQuery('#itemQuality').html('1');
+    }else{
+        jQuery('#itemQuality').html('0');
+    }
+}
 function addToCart(index) {
     var storage = window.localStorage;
     var cartProducts =  storage.getItem("products");
@@ -15,13 +28,22 @@ function addToCart(index) {
         cartProducts = index;
     }
     storage.setItem("products", cartProducts);
-    jQuery('.itemQuality').html(window.localStorage.getItem("products").split(",").length -1);
+    updateQuatityInCart();
     jQuery('#addToCartModel').modal('show');
+}
+
+function removeFromCart(index) {
+    var storage = window.localStorage;
+    var cartProducts =  storage.getItem("products");
+    if(cartProducts != null && cartProducts.indexOf(index+',') != -1){
+        cartProducts = cartProducts.replace(index + ',', '');
+    }else{
+        cartProducts = cartProducts.replace(index, '');
+    }
+    storage.setItem("products", cartProducts);
+    jQuery('#itemQuality').html(window.localStorage.getItem("products").split(",").length -1);
 }
 function continueShopping() {
     jQuery('#addToCartModel').modal('hide');
 }
 
-jQuery(document).ready(function () {
-    jQuery('.itemQuality').html(window.localStorage.getItem("products").split(",").length -1);
-});
--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,3 +1,172 @@
+var productsDefinition = [{
+    "name": "LOREM IPSUM DOLOR 15",
+    "price": "33900",
+    "image":"photo0.jpg"
+},
+{
+    "name":"LOREM IPSUM DOLOR 1",
+    "price": "28500",
+    "image":"photo1.jpg"
+}];
+var totalSum = 0;
+jQuery(document).ready(function () {
+    var cartProducts  = window.localStorage.getItem("products");
+    if(cartProducts != null){
+        var products = cartProducts.split(",");
+        var tbody = jQuery('#cartTable').find('tbody');
+        var list = new Array();
+        totalSum = 0;
+        for (var i = 0; i < products.length; i++) {
+            if(products[i] == ""){
+                continue;
+            }
+            var price = productsDefinition[products[i]].price;
+            if(list[products[i]] != null){
+                list[products[i]] = list[products[i]] + 1;
+                jQuery('#quatity'+products[i]).val(list[products[i]]);
+                jQuery('#sum'+products[i]).html(formatPrice(list[products[i]]*price));
+            }else{
+                var productName = productsDefinition[products[i]].name;
+                var image = productsDefinition[products[i]].image;
+
+                console.log(productName)
+                tbody.append('<tr valign="top" class="sectiontableentry1" id="row'+products[i]+'"><td align="left">' +
+                    '<span class="cart-images"><img src="./assets/images/'+image+'"></span><a href="" class="cart-item-link">'+productName+'</a>' +
+                    '<div class="vm-customfield-cart"></div></td><td align="center"><div class="PricesalesPrice vm-display vm-price-value">' +
+                    '<span class="vm-price-desc"></span><span class="PricesalesPrice">'+formatPrice(price)+'</span></div></td><td align="right">' +
+                    '<div class="cart-product__quantity"><div class="quantity-form"><div><button title="Decrease" onclick="decreaseQuatity(\''+products[i]+'\')" class="quantity-form__decrease-button"></button>' +
+                    '<input type="number" min="0" aria-label="Antal" class="quantity-form__quantity-value" value="1" id="quatity'+products[i]+'">' +
+                    '<button title="Increase" class="quantity-form__increase-button" onclick="increaseQuatity(\''+products[i]+'\')"></button></div><button onclick="removeProduct(\''+products[i]+'\')" title="Remove" class="quantity-form__delete-button"></button>' +
+                    '</div></div></td><td colspan="1" align="right"><div class="PricesalesPrice vm-display vm-price-value"><spanclass="vm-price-desc"></span>' +
+                    '<span class="PricesalesPrice" id="sum'+products[i]+'">'+formatPrice(price)+'</span></div></td></tr>');
+                list[products[i]] = 1;
+            }
+            totalSum += parseInt(price);
+        }
+        tbody.append('<tr class="sectiontableentry2">\n' +
+            '                                                <td colspan="3" align="right">Total:</td>\n' +
+            '\n' +
+            '                                                <td align="right"><strong>\n' +
+            '                                                    <div class="PricebillTotal vm-display vm-price-value"><span\n' +
+            '                                                            class="vm-price-desc"></span><span class="PricebillTotal">'+formatPrice(totalSum)+'</span>\n' +
+            '                                                    </div>\n' +
+            '                                                </strong></td>\n' +
+            '                                            </tr>');
+    }
+
+});
+
+function increaseQuatity(product) {
+    var quatity = jQuery('#quatity'+product).val();
+    jQuery('#quatity'+product).val(parseInt(quatity) +1);
+    var storage = window.localStorage;
+    var cartProducts =  storage.getItem("products");
+    console.log(cartProducts);
+    if(cartProducts != null && cartProducts != ''){
+        cartProducts = cartProducts + "," + product;
+    }else {
+        cartProducts = index;
+    }
+    storage.setItem("products", cartProducts);
+    updateQuatityInCart();
+    var price = productsDefinition[product].price;
+    jQuery('#sum'+product).html(formatPrice((parseInt(quatity)+1)*price));
+    totalSum += parseInt(price);
+    jQuery('.PricebillTotal').html(formatPrice(totalSum));
+    if(jQuery('#installmentsDiv').is(':visible')){
+        selectOtherPayment();
+    }
+
+}
+
+function decreaseQuatity(product) {
+    var quatity = jQuery('#quatity'+product).val();
+    if(quatity == '1'){
+        removeProduct(product);
+        return;
+    }
+    jQuery('#quatity'+product).val(parseInt(quatity) -1);
+    var storage = window.localStorage;
+    var cartProducts =  storage.getItem("products");
+    console.log(cartProducts);
+    if(cartProducts != null && cartProducts.indexOf(product + ',') != -1){
+        cartProducts = cartProducts.replace(product + ',', '');
+        console.log(cartProducts);
+    }else {
+        cartProducts = cartProducts.replace(product, '');
+        console.log(cartProducts);
+    }
+    storage.setItem("products", cartProducts);
+    updateQuatityInCart();
+    var price = productsDefinition[product].price;
+    jQuery('#sum'+product).html(formatPrice((parseInt(quatity)-1)*price));
+    totalSum -= parseInt(price);
+    jQuery('.PricebillTotal').html(formatPrice(totalSum));
+    if(jQuery('#installmentsDiv').is(':visible')){
+        selectOtherPayment();
+    }
+}
+
+function removeProduct(product) {
+    var quatity = jQuery('#quatity'+product).val();
+    jQuery('#row'+product).remove();
+    var storage = window.localStorage;
+    var cartProducts =  storage.getItem("products");
+    while(cartProducts != null && cartProducts.indexOf(product + ',') != -1){
+        cartProducts = cartProducts.replace(product + ',', '');
+    }
+    while (cartProducts != null && cartProducts.indexOf(','+product) != -1){
+        cartProducts = cartProducts.replace(','+product,'');
+    }
+    while (cartProducts != null && cartProducts.indexOf(product) != -1){
+        cartProducts = cartProducts.replace(product,'');
+    }
+
+
+    if(cartProducts != ''){
+        storage.setItem("products", cartProducts);
+    }else{
+        storage.clear();
+    }
+
+    jQuery('#itemQuality').html(cartProducts.split(",").length);
+    var price = productsDefinition[product].price;
+    totalSum -= parseInt(price*quatity);
+    jQuery('.PricebillTotal').html(formatPrice(totalSum));
+    if(jQuery('#installmentsDiv').is(':visible')){
+        selectOtherPayment();
+    }
+}
+
+function formatPrice(price) {
+    return number_format(price,2,',', ' ') + ' kr';
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 2 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function(n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + Math.ceil(n * k) / k;
+        };
+
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    var re = /(-?\d+)(\d{3})/;
+    while(re.test(s[0])) {
+        s[0] = s[0].replace(re, "$1" + sep + "$2");
+    }
+
+    if((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+}
+
 function clickLoginButton(){
     jQuery('#addressDiv').show();
     jQuery('#loginForm').hide();
@@ -14,23 +183,42 @@ function show6monthfee() {
     jQuery('#rate1').html('5.8%');
     jQuery('#rate2').html('5.5%');
     jQuery('#rate3').html('6.5%');
-    jQuery('#mc1').html('3170 sek/month');
-    jQuery('#mc2').html('3161 sek/month');
-    jQuery('#mc3').html('3191 sek/month');
-
-
+    jQuery('#mc6').html(totalSum/6+' sek/month');
+    jQuery('#mc12').html(totalSum/12+' sek/month');
+    jQuery('#mc1').html(totalSum/6+' sek/month');
+    jQuery('#mc2').html(totalSum/6+'sek/month');
+    jQuery('#mc3').html(totalSum/6+' sek/month');
 }
 
 function show12monthfee() {
     jQuery('#rate1').html('3.8%');
     jQuery('#rate2').html('3.5%');
     jQuery('#rate3').html('4.5%');
-    jQuery('#mc1').html('1555 sek/month');
-    jQuery('#mc2').html('1550 sek/month');
-    jQuery('#mc3').html('1565 sek/month');
+    jQuery('#mc12').html(totalSum/12+' sek/month');
+    jQuery('#mc1').html(totalSum/12+' sek/month');
+    jQuery('#mc2').html(totalSum/12+' sek/month');
+    jQuery('#mc3').html(totalSum/12+' sek/month');
+}
+function clickChooseBank() {
+    jQuery(".progress-bar-info").css('width','0%');
+    jQuery('#progressBar').modal('show');
+    jQuery('#progressBarText').html('Processing your application.....');
+    var i=1;
+    var interval = setInterval(function(){
+        jQuery(".progress-bar-info").css('width', (10*i++)+'%');
+        jQuery('#progressBarText').html('Processing your application ' + 10*(i-1) + '% completed.....');
+        if(i==11){
+            jQuery('#progressBar').modal('hide');
+            jQuery('#activePayment').hide();
+            clearInterval(interval);
+            window.location.href='pnsign.html';
+        }
+    }, 1000);
+
 }
 
 function installmentsClick(){
+    jQuery('.payment_options').removeClass('selected');
     jQuery(".progress-bar-info").css('width','0%');
     jQuery('#progressBar').modal('show');
     jQuery('#progressBarText').html('Calculating offerings.....');
@@ -42,12 +230,16 @@ function installmentsClick(){
             jQuery('#progressBar').modal('hide');
             jQuery('#activePayment').hide();
             jQuery('#installmentsDiv').show();
+            show6monthfee();
             clearInterval(interval);
         }
     }, 1000);
 }
 
-
+function selectPaymentOption(src){
+    jQuery('.payment_options').removeClass('selected');
+    src.addClass('selected');
+}
 
 jQuery(function ($) {
             SqueezeBox.initialize({});
